8dc30c8ce1e28370daa0b365279a0629dba06417
secretmoo35 <sarayut-moo.th@hotmail.com>
2016-01-20 18:09:59 +0700

    EmployeeData

--- www/js/controllers/homeController.js
+++ www/js/controllers/homeController.js
@@ -1,4 +1,12 @@
-myApp.controller('homeController', ['$scope', function($scope) {
+myApp.controller('homeController', ['$scope', 'employeeService', function($scope, employeeService) {
 
+    $scope.employeeData = [];
 
+    employeeService.getEmployeeByEmail().then(function(results) {
+
+        $scope.employeeData = results.data;
+
+    }, function(error) {
+        alert(error.data.message);
+    })
 }]);
