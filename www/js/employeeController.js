2e381d76f0b9a53d98667768c3a5dc7c5efac6e2
tingtang35 <tingtang35@gmail.com>
2016-01-20 16:02:24 +0700

    update Profile

---
+++ www/js/employeeController.js
@@ -0,0 +1,13 @@
+myApp.controller('employeeController', ['$scope', function($scope) {
+
+    // $scope.tabname = "1";
+    //         $scope.selectTab = function(tab) {
+    //             $scope.tabname = tab;
+    //         };
+    $scope.var = 1;
+    $scope.click = function(num) {
+        $scope.var = num;
+    }
+
+
+}]);
